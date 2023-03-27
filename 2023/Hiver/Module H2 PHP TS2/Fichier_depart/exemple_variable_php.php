4dde88b1d18efe54c6e85b28f0fe499f4beaac12
richard-phat-the-nguyen <richardphat@gmail.com>
2023-03-27 07:01:08 -0400

    Update_0701_2023_03_27

---
+++ 2023/Hiver/Module H2 PHP TS2/Fichier_depart/exemple_variable_php.php
@@ -0,0 +1,102 @@
+<?php
+$tete='<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<div class="w3-container w3-teal">
+<h1>Summer Holiday</h1>
+</div>';
+
+
+$corps='<div class="w3-content">
+
+		<div class="w3-row w3-margin">
+
+		<div class="w3-third">
+		  <img src="https://www.w3schools.com/w3css/img_5terre.jpg" style="width:100%;min-height:200px">
+		</div>
+		<div class="w3-twothird w3-container">
+		  <h2>5 Terre</h2>
+		  <p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		  </p>
+		</div>
+
+		</div>
+		<div class="w3-row w3-margin">
+
+		<div class="w3-third">
+		  <img src="https://www.w3schools.com/w3css/img_monterosso.jpg" style="width:100%;min-height:200px">
+		</div>
+		<div class="w3-twothird w3-container">
+		  <h2>Monterosso</h2>
+		  <p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		  </p>
+		</div>
+
+		</div>
+		<div class="w3-row w3-margin">
+
+		<div class="w3-third">
+		  <img src="https://www.w3schools.com/w3css/img_vernazza.jpg" style="width:100%;min-height:200px">
+		</div>
+		<div class="w3-twothird w3-container">
+		  <h2>Vernazza</h2>
+		  <p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		  </p>
+		</div>
+
+		</div>
+		<div class="w3-row w3-margin">
+
+		<div class="w3-third">
+		  <img src="https://www.w3schools.com/w3css/img_manarola.jpg" style="width:100%;min-height:200px">
+		</div>
+		<div class="w3-twothird w3-container">
+		  <h2>Manarola</h2>
+		  <p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		  </p>
+		</div>
+
+		</div>
+		<div class="w3-row w3-margin">
+
+		<div class="w3-third">
+		  <img src="https://www.w3schools.com/w3css/img_corniglia.jpg" style="width:100%;min-height:200px">
+		</div>
+		<div class="w3-twothird w3-container">
+		  <h2>Corniglia</h2>
+		  <p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		  </p>
+		</div>
+
+		</div>
+		<div class="w3-row w3-margin">
+
+		<div class="w3-third">
+		  <img src="https://www.w3schools.com/w3css/img_riomaggiore.jpg" style="width:100%;min-height:200px">
+		</div>
+		<div class="w3-twothird w3-container">
+		  <h2>Riomaggiore</h2>
+		  <p>
+		  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		  </p>
+		</div>
+
+		</div>
+</div>';
+
+$pied='</body>
+</html>';
+
+print $tete;
+print $corps;
+print $pied;
+
+?>
